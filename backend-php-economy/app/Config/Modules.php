f39ab824387dcf3265a99ccbddca0504b7720b3f
ELEV8 Deploy <deploy@elev8.local>
2026-03-25 23:40:07 +0530

    Fix: Explicitly require BaseConfig before extending in Modules.php to fix fatal un-autoloaded crash

--- backend-php-economy/app/Config/Modules.php
+++ backend-php-economy/app/Config/Modules.php
@@ -2,6 +2,10 @@
 
 namespace Config;
 
+if (! class_exists(\CodeIgniter\Config\BaseConfig::class, false)) {
+    require_once SYSTEMPATH . 'Config/BaseConfig.php';
+}
+
 use CodeIgniter\Modules\Modules as BaseModules;
 
 class Modules extends BaseModules
