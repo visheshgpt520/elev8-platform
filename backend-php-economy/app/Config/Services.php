748d85692b15bd4e016c65f26c3552e0803c684a
ELEV8 Deploy <deploy@elev8.local>
2026-03-25 23:45:49 +0530

    Fix: Add missing app/Config/Services.php required by CodeIgniter 4 boot sequence

---
+++ backend-php-economy/app/Config/Services.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Config;
+
+use CodeIgniter\Config\BaseService;
+
+/**
+ * Services Configuration file.
+ *
+ * Services are simply other classes/libraries that the system uses
+ * to do its job. This is used by CodeIgniter to allow the core of the
+ * framework to be swapped out easily without affecting the usage within
+ * the rest of your application.
+ */
+class Services extends BaseService
+{
+    // Placeholder for custom services
+}
